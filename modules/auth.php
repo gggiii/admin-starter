49f609e772e3571ca01dba06d8e8e03518f08c8f
Patrik Krivulčík <krivulcikp@gmail.com>
2020-06-28 10:43:52 +0200

    Auth login function finish

--- modules/auth.php
+++ modules/auth.php
@@ -1,59 +1,56 @@
 <?php
-    class Auth{
-        function __construct($db) {
-            
-            $this->db = $db;
-            $this->usersTable = 'users';
-            $this->sessionName = 'adminLogin';
-            $this->multipleSessions = false;
+class Auth
+{
+    function __construct($db)
+    {
 
+        $this->db = $db;
+        $this->usersTable = 'users';
+        $this->sessionName = 'adminLogin';
+        $this->sessionExpiration = 200; //seconds
+        $this->multipleSessions = false;
+        $this->overrideUserSession = false;
+    }
 
-        }
+    function login($username, $password)
+    {
 
-        function login($username, $password){
-            
-            $users = $this->db->query('SELECT * FROM ' . $this->usersTable . ' WHERE USERNAME=?', $username)->fetchAll();
-            if( count($users) == 0){
+        $users = $this->db->query('SELECT * FROM ' . $this->usersTable . ' WHERE USERNAME=?', $username)->fetchAll();
+        if (count($users) == 0) {
+            return array(
+                'error' => 'no-user',
+                'message' => 'User does not exist'
+            );
+        } else {
+            if (!password_verify($password, $users[0]['password'])) {
                 return array(
-                    'error'=> 'no-user',
-                    'message' => 'User does not exist'
+                    'error' => 'wrong-pass',
+                    'message' => 'Wrong user password'
                 );
-            }else{
-                if( ! password_verify($password,$users[0]['password']) ){
-                    return array(
-                        'error'=> 'wrong-pass',
-                        'message' => 'Wrong user password'
-                    );
-                }else{
-                    if($this->multipleSessions){
-                        //TODO:Add functionality
-                    }else{
+            } else {
+                if ($this->multipleSessions) {
+                    //TODO:Add functionality
+                } else {
+                   
+                    $newHash = md5($_COOKIE['PHPSESSID'] . $_SERVER['HTTP_USER_AGENT']);
+                    $sinceLastVerified = time() - strtotime($users[0]['lastVerified']);
 
-                        if( $users[0]['hash'] == ''){
-                            $newHash = md5($_COOKIE['PHPSESSID'].$_SERVER['HTTP_USER_AGENT']);
-                            $this->db->query('UPDATE ' . $this->usersTable . ' SET hash=?,lastVerified=now() WHERE id=?', $newHash, $users[0]['id']);
+                    if ($sinceLastVerified > $this->sessionExpiration || $this->overrideUserSession) {
+                        //UPDATE SESSION INFO
+                        $this->db->query('UPDATE ' . $this->usersTable . ' SET hash=?,lastVerified=now() WHERE id=?', $newHash, $users[0]['id']);
+                        $_SESSION[$this->sessionName] = $users[0]['id'];
+                        return true;
 
-                            $_SESSION[$this->sessionName] = $users[0]['id'];
-                            return true;
-                        }else{
-                            if(md5($_COOKIE['PHPSESSID'].$_SERVER['HTTP_USER_AGENT']) == $users[0]['hash']){
-                                $this->db->query('UPDATE ' . $this->usersTable . ' SET lastVerified=now() WHERE id=?', $users[0]['id']);
-                                return true;
-                            }else{
-                                return array(
-                                    'error'=> 'sesssion-allready-running',
-                                    'message' => 'Another user session is allready running'
-                                );
-                            }
-                            
-                        }
+                    } else {
+                        //OLDER SESSION STILL LASTS
+                        return array(
+                            'error' => 'session-running',
+                            'message' => 'A login session is allready running'
+                        );
+                        
                     }
-                    
                 }
             }
-
         }
-
-        
     }
-?>
+}
