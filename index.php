9731901aae7adb2d94964da696621d34c621ad09
Patrik Krivulčík <krivulcikp@gmail.com>
2020-06-28 15:06:51 +0200

    Added auth functions

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
-
+<pre>
 <?php
-    include 'app.php';
-    $admin = new Admin();
-
-    print_r($admin->auth->login('test','test'));
-   
+include 'app.php';
+$admin = new Admin();
+// print_r($admin->auth->logout());
+//print_r(openssl_get_cipher_methods());
+// print_r($admin->auth->login('test','test'));
+print_r($admin->auth->getUsername());
 ?>
-
+</pre>
--- modules/auth.php
+++ modules/auth.php
@@ -6,13 +6,20 @@ class Auth
 
         $this->db = $db;
         $this->usersTable = 'users';
-        $this->sessionName = 'adminLogin';
-        $this->sessionExpiration = 200; //seconds
+        $this->settingsTable = 'settings';
+
+        $this->sessionUserId = 'adminLoginId';
+        $this->sessionUserData = 'adminData';
+        $this->sessionExpiration = 2000; //seconds
         $this->multipleSessions = false;
         $this->overrideUserSession = false;
+
+        $this->encryptionKey = 'mykey123';
+        $this->encryptionMethod = 'aes-128-cbc-hmac-sha256';
+        $this->encryptionIV = hex2bin('422d7aded49d32115968698bb5fb0f9a');
     }
 
-    function login($username, $password)
+    public function login($username, $password)
     {
 
         $users = $this->db->query('SELECT * FROM ' . $this->usersTable . ' WHERE USERNAME=?', $username)->fetchAll();
@@ -31,26 +38,123 @@ class Auth
                 if ($this->multipleSessions) {
                     //TODO:Add functionality
                 } else {
-                   
+
                     $newHash = md5($_COOKIE['PHPSESSID'] . $_SERVER['HTTP_USER_AGENT']);
                     $sinceLastVerified = time() - strtotime($users[0]['lastVerified']);
 
-                    if ($sinceLastVerified > $this->sessionExpiration || $this->overrideUserSession) {
+                    if (($sinceLastVerified > $this->sessionExpiration || $this->overrideUserSession) ||  !isset($_SESSION[$this->sessionUserId])) {
                         //UPDATE SESSION INFO
                         $this->db->query('UPDATE ' . $this->usersTable . ' SET hash=?,lastVerified=now() WHERE id=?', $newHash, $users[0]['id']);
-                        $_SESSION[$this->sessionName] = $users[0]['id'];
+                        $_SESSION[$this->sessionUserId] = $users[0]['id'];
+                        //ENCRYPT DATA
+                        $userData = openssl_encrypt(json_encode($users[0]), $this->encryptionMethod, $this->encryptionKey, $options = 0, $this->encryptionIV);
+                        $_SESSION[$this->sessionUserData] = $userData;
                         return true;
-
                     } else {
                         //OLDER SESSION STILL LASTS
                         return array(
                             'error' => 'session-running',
                             'message' => 'A login session is allready running'
                         );
-                        
                     }
                 }
             }
+        }
+    }
+
+    public function logout()
+    {
+        // unset($_SESSION[$this->sessionUserId]);
+        $_SESSION[$this->sessionUserId] = '';
+        return true;
+    }
+
+    public function validateLogin($checkExpired = false)
+    {
+        if (!isset($_SESSION[$this->sessionUserId])) {
+            return false;
+        } else {
+            //GET HASH FROM DB
+
+            $myHash = md5($_COOKIE['PHPSESSID'] . $_SERVER['HTTP_USER_AGENT']);
+            $user = $this->db->query('SELECT id,lastVerified FROM ' . $this->usersTable . ' WHERE hash=?', $myHash)->fetchArray();
+            if (
+                count($user) == 0 ||
+                $user['id'] != $_SESSION[$this->sessionUserId] ||
+                ($checkExpired &&
+                    time() - strtotime($user['lastVerified']) > $this->sessionExpiration)
+            ) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+    }
+
+
+
+    public function getRole()
+    {
+        if ($this->validateLogin(true)) {
+            $data = $_SESSION[$this->sessionUserData];
+            $data = openssl_decrypt($data, $this->encryptionMethod, $this->encryptionKey, $options = 0, $this->encryptionIV);
+            return json_decode($data, 1)['role'];
+        } else {
+            return array(
+                'error' => 'no-login',
+                'message' => 'No user login'
+            );
+        }
+    }
+    public function getPermissions()
+    {
+        if ($this->validateLogin(true)) {
+            $data = $_SESSION[$this->sessionUserData];
+            $data = openssl_decrypt($data, $this->encryptionMethod, $this->encryptionKey, $options = 0, $this->encryptionIV);
+            $role = json_decode($data, 1)['role'];
+
+            //GET ROLE PERMISSION
+            $roles = $this->db->query('SELECT cont FROM ' . $this->settingsTable . ' WHERE name=?', 'auth_roles')->fetchArray();
+            if (isset(json_decode($roles['cont'], 1)[$role])) {
+
+                return json_decode($roles['cont'], 1)[$role]['permissions'];
+            } else {
+                return array(
+                    'error' => 'no-role',
+                    'message' => "User's role does not exist"
+                );
+            }
+        } else {
+            return array(
+                'error' => 'no-login',
+                'message' => 'No user login'
+            );
+        }
+    }
+    public function getUsername()
+    {
+        if ($this->validateLogin(true)) {
+            $data = $_SESSION[$this->sessionUserData];
+            $data = openssl_decrypt($data, $this->encryptionMethod, $this->encryptionKey, $options = 0, $this->encryptionIV);
+            return json_decode($data, 1)['username'];
+        } else {
+            return array(
+                'error' => 'no-login',
+                'message' => 'No user login'
+            );
+        }
+    }
+    public function getId()
+    {
+        if ($this->validateLogin(true)) {
+            $data = $_SESSION[$this->sessionUserData];
+            $data = openssl_decrypt($data, $this->encryptionMethod, $this->encryptionKey, $options = 0, $this->encryptionIV);
+            return json_decode($data, 1)['id'];
+        } else {
+            return array(
+                'error' => 'no-login',
+                'message' => 'No user login'
+            );
         }
     }
 }
