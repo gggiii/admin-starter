6a864fa9ecf6f59625f43bff80d27c4d5389e064
Patrik Krivulčík <krivulcikp@gmail.com>
2020-07-02 18:33:12 +0200

    Added file upload for multiple files

--- index.php
+++ index.php
@@ -1,18 +1,20 @@
 <pre>
+<form action="#" method="POST" enctype="multipart/form-data">
 <?php
 include 'app.php';
 $app = new Admin();
 
-
-if(count($_FILES) > 0){    
-    $app->items->add(array('new-name'=>'asdasd'), $_FILES);
+if (isset($_FILES['new-file'])) {
+    print_r($app->items->add(array(),$_FILES));
 }
-?>
-<form action="#" method="POST" enctype="multipart/form-data">
-<?php
-    echo $app->items->getEditField(1,'name', false,'asd');
+
+
+
+//$app->items->edit(1, array('edit-name'=>'asdasd', 'edit-number'=>'1000'))
+echo $app->items->getNewField('file');
 ?>
 <input type="submit">
+
 </form>
 </pre>
 <script src="ckeditor/ckeditor.js"></script>
--- modules/items.php
+++ modules/items.php
@@ -11,6 +11,22 @@ class Items
     {
         $this->db = $db;
         $this->itemsTable = 'items';
+        /**
+         * Types
+         *      simple-text
+         *          max-length int max characters
+         *      simple-number
+         *      textarea
+         *      ckeditor
+         *      select
+         *          options array of options
+         *      file
+         *          accept string of accepted filetypes, the accept attribute of an input field
+         *          uploadPath string path to upload to, ending with /
+         *          single boolean if only one file is allowed
+         * 
+         * 
+         */
         $this->fields = array(
             'name' => array(
                 'type' => 'simple-text',
@@ -18,8 +34,8 @@ class Items
             ),
             'file' => array(
                 'type' => 'file',
-                'accept' => 'image/*',
-                'uploadPath'=> './uploads/'
+                'uploadPath' => 'uploads/',
+                'multiple' => 'multiple'
             )
         );
         $this->newFieldPrefix = 'new-';
@@ -38,7 +54,8 @@ class Items
             return $this->db->query('SELECT * FROM ' . $this->itemsTable . ' WHERE id=?', $id)->fetchArray();
         }
     }
-    public function add($post ,$files = false)
+
+    public function add($post, $files = false)
     {
 
         $keys = array_keys($post);
@@ -53,21 +70,35 @@ class Items
                     'message' => 'Some fields do not contain the correct newFieldPrefix'
                 );
             } else {
-                    $query .= explode($this->newFieldPrefix, $key)[1] . ',';
-                    $values .= '?,';
-                    $valuesActual[] = $post[$key];
-                }
-            
+                $query .= explode($this->newFieldPrefix, $key)[1] . ',';
+                $values .= '?,';
+                $valuesActual[] = $post[$key];
+            }
         }
-        if($files !== false){
-            for ($i=0; $i < count($files); $i++) { 
-                $file = $files[array_keys($files)[$i]];
-                if(move_uploaded_file($file['tmp_name'],
-                $this->fields[explode($this->newFieldPrefix, array_keys($files)[$i])[1]]['uploadPath'] . $file['name'])){
-
-                    $query .= explode($this->newFieldPrefix, array_keys($files)[$i])[1] . ',';
+        if ($files !== false) {
+            foreach ($files as $name => $file) {
+                if ($this->fields[explode($this->newFieldPrefix, $name)[1]]['multiple'] != 'multiple') {
+                    //IF IS CONFIGURED FOR A SINGLE FILE
+                    if (move_uploaded_file(
+                        $file['tmp_name'],
+                        $this->fields[explode($this->newFieldPrefix, $name)[1]]['uploadPath'] . $file['name']
+                    )) {
+                        $query .= explode($this->newFieldPrefix, $name)[1] . ',';
+                        $values .= '?,';
+                        $valuesActual[] = json_encode(array($file['name']));
+                    }
+                } else {
+                    $dafiles = array();
+                    for ($i = 0; $i < count($file['name']); $i++) {
+                        move_uploaded_file(
+                            $file['tmp_name'][$i],
+                            $this->fields[explode($this->newFieldPrefix, $name)[1]]['uploadPath'] . $file['name'][$i]
+                        );
+                        $dafiles[] = $file['name'][$i];
+                    }
+                    $query .= explode($this->newFieldPrefix, $name)[1] . ',';
                     $values .= '?,';
-                    $valuesActual[] = $file['name'];
+                    $valuesActual[] = json_encode($dafiles);
                 }
             }
         }
@@ -129,7 +160,7 @@ class Items
 
             case 'file':
                 $accept  = (isset($field['accept'])) ? 'accept="' . $field['accept'] . '"' : '';
-                $output = '<input type="file" name="' . $this->newFieldPrefix . $fieldName . '"id="' . $this->newFieldPrefix . $fieldName . '" ' . $accept . '>';
+                $output = '<input type="file" name="' . $this->newFieldPrefix . $fieldName . (($field['multiple'] == 'multiple')?'[]':'') .'"id="' . $this->newFieldPrefix . $fieldName . '" ' . $accept . $field['multiple'] . '>';
                 break;
 
 
@@ -140,64 +171,103 @@ class Items
         return $output;
     }
 
-    public function getEditField($id,$fieldIdentifier, $placeholder = false, $value = false)
-        {
-            if (is_int($fieldIdentifier)) {
-                $fieldName = array_keys($this->fields)[$fieldIdentifier];
+    public function getEditField($fieldIdentifier, $placeholder = false, $value = false)
+    {
+        if (is_int($fieldIdentifier)) {
+            $fieldName = array_keys($this->fields)[$fieldIdentifier];
+        } else {
+            if (!isset($this->fields[$fieldIdentifier])) {
+                return array(
+                    'error' => 'no-field',
+                    'message' => 'No such field exists'
+                );
             } else {
-                if (!isset($this->fields[$fieldIdentifier])) {
-                    return array(
-                        'error' => 'no-field',
-                        'message' => 'No such field exists'
-                    );
-                } else {
-                    $fieldName = $fieldIdentifier;
+                $fieldName = $fieldIdentifier;
+            }
+        }
+        $field = $this->fields[$fieldName];
+        $output = '';
+        switch ($field['type']) {
+            case 'simple-text':
+                $maxlength = (isset($field['maxlength'])) ? 'maxlength="' . $field['maxlength'] . '"' : '';
+                $output = '<input type="text" name="' . $this->editFieldPrefix . $fieldName . '"id="' . $this->editFieldPrefix . $fieldName . '" placeholder="' . (($placeholder !== false) ? $placeholder : '') . '" ' . $maxlength . '  value="' . (($value !== false) ? $value : '') . '">';
+                break;
+
+
+            case 'simple-number':
+                $output = '<input type="number" name="' . $this->editFieldPrefix . $fieldName . '"id="' . $this->editFieldPrefix . $fieldName . '" placeholder="' . (($placeholder !== false) ? $placeholder : '') . '" value="' . (($value !== false) ? $value : '') . '">';
+                break;
+
+
+            case 'textarea':
+                $output = '<textarea name="' . $this->editFieldPrefix . $fieldName . '"id="' . $this->editFieldPrefix . $fieldName . '" placeholder="' . (($placeholder !== false) ? $placeholder : '') . '" value="' . (($value !== false) ? $value : '') . '"></textarea>';
+                break;
+
+
+            case 'ckeditor':
+                $output = '<textarea class="ckeditor" name="' . $this->editFieldPrefix . $fieldName . '"id="' . $this->editFieldPrefix . $fieldName . '" placeholder="' . (($placeholder !== false) ? $placeholder : '') . '"  value="' . (($value !== false) ? $value : '') . '"></textarea>';
+                break;
+
+
+            case 'select':
+                $output = '<select name="' . $this->editFieldPrefix . $fieldName . '"id="' . $this->editFieldPrefix . $fieldName . '"  value="' . (($value !== false) ? $value : '') . '">';
+                for ($i = 0; $i < count($field['options']); $i++) {
+                    $option = $field['options'][$i];
+                    $output .= '<option value="' . $option . '">' . $option . '</option>';
+                }
+                $output .= '</select>';
+                break;
+
+
+            case 'file':
+                $accept  = (isset($field['accept'])) ? 'accept="' . $field['accept'] . '"' : '';
+                $output = '<input type="file" name="' . $this->editFieldPrefix . $fieldName . (($field['multiple'] == 'multiple')?'[]':'') . '"id="' . $this->editFieldPrefix . $fieldName . '" ' . $accept . $field['multiple'] . '>';
+                break;
+
+
+            default:
+                # code...
+                break;
+        }
+        return $output;
+    }
+
+    public function edit($id, $post, $files = false)
+    {
+        $keys = array_keys($post);
+        $query = 'UPDATE ' . $this->itemsTable . ' SET ';
+        $values = '';
+        $valuesActual = array();
+        for ($i = 0; $i < count($keys); $i++) {
+            $key = $keys[$i];
+            if (strpos($key, $this->editFieldPrefix) === false) {
+                return array(
+                    'error' => 'wrong-fields-format',
+                    'message' => 'Some fields do not contain the correct editFieldPrefix'
+                );
+            } else {
+                $query .= explode($this->editFieldPrefix, $key)[1] . '=?,';
+                $valuesActual[] = $post[$key];
+            }
+        }
+        if ($files !== false) {
+            for ($i = 0; $i < count($files); $i++) {
+                $file = $files[array_keys($files)[$i]];
+                if (move_uploaded_file(
+                    $file['tmp_name'],
+                    $this->fields[explode($this->editFieldPrefix, array_keys($files)[$i])[1]]['uploadPath'] . $file['name']
+                )) {
+
+                    $query .= explode($this->editFieldPrefix, array_keys($files)[$i])[1] . '=?,';
+                    $valuesActual[] = json_encode(array($file['name']));
                 }
             }
-            $field = $this->fields[$fieldName];
-            $output = '';
-            switch ($field['type']) {
-                case 'simple-text':
-                    $maxlength = (isset($field['maxlength'])) ? 'maxlength="' . $field['maxlength'] . '"' : '';
-                    $output = '<input type="text" name="' . $this->editFieldPrefix . $fieldName . '"id="' . $this->editFieldPrefix . $fieldName . '" placeholder="' . (($placeholder !== false) ? $placeholder : '') . '" ' . $maxlength . '  value="' . (($value !== false) ? $value : '') . '">';
-                    break;
-    
-    
-                case 'simple-number':
-                    $output = '<input type="number" name="' . $this->editFieldPrefix . $fieldName . '"id="' . $this->editFieldPrefix . $fieldName . '" placeholder="' . (($placeholder !== false) ? $placeholder : '') . '" value="' . (($value !== false) ? $value : '') . '">';
-                    break;
-    
-    
-                case 'textarea':
-                    $output = '<textarea name="' . $this->editFieldPrefix . $fieldName . '"id="' . $this->editFieldPrefix . $fieldName . '" placeholder="' . (($placeholder !== false) ? $placeholder : '') . '" value="' . (($value !== false) ? $value : '') . '"></textarea>';
-                    break;
-    
-    
-                case 'ckeditor':
-                    $output = '<textarea class="ckeditor" name="' . $this->editFieldPrefix . $fieldName . '"id="' . $this->editFieldPrefix . $fieldName . '" placeholder="' . (($placeholder !== false) ? $placeholder : '') . '"  value="' . (($value !== false) ? $value : '') . '"></textarea>';
-                    break;
-    
-    
-                case 'select':
-                    $output = '<select name="' . $this->editFieldPrefix . $fieldName . '"id="' . $this->editFieldPrefix . $fieldName . '"  value="' . (($value !== false) ? $value : '') . '">';
-                    for ($i = 0; $i < count($field['options']); $i++) {
-                        $option = $field['options'][$i];
-                        $output .= '<option value="' . $option . '">' . $option . '</option>';
-                    }
-                    $output .= '</select>';
-                    break;
-    
-    
-                case 'file':
-                    $accept  = (isset($field['accept'])) ? 'accept="' . $field['accept'] . '"' : '';
-                    $output = '<input type="file" name="' . $this->editFieldPrefix . $fieldName . '"id="' . $this->editFieldPrefix . $fieldName . '" ' . $accept . '>';
-                    break;
-    
-    
-                default:
-                    # code...
-                    break;
-            }
-            return $output;
         }
+        $query = rtrim($query, ' ,') . ' WHERE id=?';
+        $valuesActual[] = $id;
+
+        $this->db->query($query, $valuesActual);
+        // return $query.json_encode($valuesActual);
+        return true;
+    }
 }
